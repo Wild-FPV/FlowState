718f50fa3d9a8c81cc7dbb1b39734a8999d02d40
skyfpv <sky.multigp@gmail.com>
2020-12-18 17:30:56 -0700

    finished #97 and #98

---
+++ Assets/Scripts/DroneControls.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DroneControls : MonoBehaviour
+{
+    public Rigidbody rb;
+    //inputs we will read from the radio
+    private float yawInput = 0;
+    private float rollInput = 0;
+    private float pitchInput = 0;
+    private float throttleInput = 0;
+
+    private Transform trans;
+
+    //properties of the player's settings
+    //To-Do: read these from a save file
+    [SerializeField] private bool midThrottle = true;
+    [SerializeField] private float superRate = 70.0f;
+    [SerializeField] private float rcRate = 90.0f;
+    [SerializeField] private float rcExpo = 0.0f;
+    [SerializeField] private bool superExpoActive = true;
+    [SerializeField] private float thrust = 25.0f;
+    //[SerializeField] private int rate = 300;
+
+    void Start()
+    {
+        //get this asset's RigidBody component
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per physics frame
+    void FixedUpdate() {
+        Joystick joystick = Joystick.current;
+        calculateThrust();
+        calculateRotation();
+    }
+
+    public void calculateThrust()
+    {
+        rb.AddRelativeForce(Vector3.up * throttleInput * thrust,ForceMode.Force);
+    }
+
+    public void calculateRotation()
+    {
+        //let's get RC values similar to what we'd see in Betaflight (aka 1000 - 2000)
+        float pitchRCValue = ((-pitchInput*0.5f)+1.5f)*1000f;
+        float yawRCValue = ((yawInput*0.5f)+1.5f)*1000f;
+        float rollRCValue = ((-rollInput*0.5f)+1.5f)*1000f;
+
+        //let's run that through the betaflight rate system to get our degrees per second setpoint for each axis
+        float pitchDPS = stickInputToDPS(pitchRCValue);
+        float yawDPS = stickInputToDPS(yawRCValue);
+        float rollDPS = stickInputToDPS(rollRCValue);
+        //Debug.Log("pitch: "+pitchDPS.ToString());
+        //Debug.Log("yaw: "+yawDPS.ToString());
+        //Debug.Log("roll: "+rollRCValue.ToString());
+        rb.angularVelocity = transform.TransformDirection(new Vector3(pitchDPS, yawDPS, rollDPS));
+        //transform.rotation = transform.rotation * Quaternion.Euler(new Vector3(pitchInput, yawInput , rollInput));
+        //-1,1 * 0.5
+        //1,3
+    }
+
+    public float stickInputToDPS(float rcData)
+    {
+        float inputValue = rcCommand(rcData, rcRate, rcExpo);
+        float angleRate = 0.0f;
+        if (superExpoActive)
+        {
+            float rcFactor = Mathf.Abs(inputValue) / (500f * rcRate / 100f);
+            rcFactor = 1f / (1f - rcFactor * superRate / 100f);
+            angleRate = rcFactor * 27f * inputValue / 16f;
+        }
+        else
+        {
+            angleRate = (superRate + 27f) * inputValue / 16f;
+        }
+        //angleRate = constrain(angleRate, -8190, 8190); // Rate limit protection
+        return angleRate/230f;
+    }
+
+    public float rcCommand(float rcData, float rcRate, float rcExpo)
+    {
+        float midRc = 1500f;
+        float tmp = Mathf.Abs(rcData - midRc) / 100f;
+        float result = ((2500f + rcExpo * (tmp * tmp - 25f)) * tmp * rcRate / 2500f);
+        if (rcData < midRc)
+        {
+            result = -result;
+        }
+        return result;
+    }
+
+    public void OnThrottle(InputValue value)
+    {
+
+
+        if(midThrottle==true){
+            throttleInput = (value.Get<float>());
+            if(throttleInput<0.0f){
+                throttleInput = 0.0f;
+            }
+        }else{
+            throttleInput = (1.0f+value.Get<float>())/2.0f;
+        }
+    }
+
+    public void OnYaw(InputValue value)
+    {
+        yawInput = value.Get<float>();
+    }
+
+    public void OnPitch(InputValue value)
+    {
+        pitchInput = value.Get<float>();
+    }
+
+    public void OnRoll(InputValue value)
+    {
+        rollInput = value.Get<float>();
+    }
+}
---
+++ Assets/Scripts/LoadMap.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+using UnityEngine.Networking;
+
+public class LoadMap : MonoBehaviour
+{
+    void Start () {
+        var myLoadedAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "standardmapelements"));
+        if (myLoadedAssetBundle == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
+
+        var mapFile = ReadMap();
+        FSMap map = JsonUtility.FromJson<FSMap>(mapFile);
+
+        List<MapAsset> mapAssets = map.assets;
+        foreach (var asset in mapAssets) {
+
+            string assetName = asset.n;
+            Debug.Log("added new asset: "+assetName);
+            List<float> assetPosition = asset.p;
+            List<float> assetOrientation = asset.o;
+            List<float> assetScale = asset.s;
+            var newAsset = myLoadedAssetBundle.LoadAsset<GameObject>(assetName);
+
+            if(newAsset!=null){
+                GameObject assetInstance = (GameObject)Instantiate(newAsset);
+                Debug.Log("added "+assetName);
+                assetInstance.transform.position = new Vector3(assetPosition[0]*0.1F, assetPosition[2]*0.1F, assetPosition[1]*0.1F);
+                assetInstance.transform.eulerAngles  = new Vector3(assetOrientation[0], assetOrientation[2], assetOrientation[1]);
+                assetInstance.transform.localScale = new Vector3(assetScale[0], assetScale[2], assetScale[1]);
+                if(assetName=="asset launch pad"){
+                    GameObject drone = GameObject.Find("Test Drone Switchback");
+                    drone.transform.position = new Vector3(assetPosition[0]*0.1F, (assetPosition[2]*0.1F)+0.5F, assetPosition[1]*0.1F);
+                    drone.transform.eulerAngles  = new Vector3(assetOrientation[0], assetOrientation[2], assetOrientation[1]);
+                }
+            }else{
+                Debug.Log("unable to add "+assetName);
+            }
+        }
+
+        myLoadedAssetBundle.Unload(false);
+
+        /*foreach (Renderer r in GameObject.FindObjectsOfType<Renderer>())
+        {
+            foreach(Material m in r.materials){
+                Material mat = new Material(Shader.Find(m.shader.name));
+
+                mat.CopyPropertiesFromMaterial(r.material);
+
+                r.material = mat;
+            }
+
+        }*/
+    }
+
+     // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    /*static string ReadString()
+    {
+        string mapName = "2020 MultiGP Global Qualifier.fmp";
+        string path = Path.Combine(Path.Combine(Application.streamingAssetsPath,"Maps"), mapName);
+
+        //Read the text from directly from the test.txt file
+        StreamReader reader = new StreamReader(path);
+        string result = reader.ReadToEnd();
+        Debug.Log(result);
+        reader.Close();
+        return result;
+    }*/
+
+    static string ReadMap()
+    {
+        var mapFile = Resources.Load("2020 MultiGP Global Qualifier");
+        Debug.Log("hello world");
+        Debug.Log(mapFile);
+        return mapFile.ToString();
+    }
+
+
+    //json serialization stuff
+    [System.Serializable]
+    public class FSMap
+    {
+        public List<MapAsset> assets;
+    }
+
+    [System.Serializable]
+    public class MapAsset
+    {
+        public string n;
+        public List<float> p;
+        public List<float> o;
+        public List<float> s;
+        public MapAssetMetadata m;
+    }
+
+    [System.Serializable]
+    public class MapAssetMetadata
+    {
+        public int id;
+        public int? checkpoint_order;
+        public bool? lap_timer;
+    }
+}
